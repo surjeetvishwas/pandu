ae107abd9ca38cd0d5a61fb9abb41c6cd2801eee
surjeetvishwas <112145615+surjeetvishwas@users.noreply.github.com>
2022-08-27 12:39:22 +0530

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,15 @@
+# a = "Surjeet Vishwas"
+
+# print('My name is ' + a)
+# print('My name is ' .format(a))
+# print(f"My name is {a}")
+
+adj= input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+super_hero_name = input("Enter your Favourite Super Hero name: ")
+
+madlib = f"Python programming is so {adj}! It makes me so excited all  the time because \
+I love to {verb1}. Stay hydrated {verb2} like you are {super_hero_name}"
+
+print(madlib)
